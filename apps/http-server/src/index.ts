fe08e87a0c0ea01f9c0a79f26121d33a86375d53
akshat09022002 <akshat09022002@gmail.com>
2025-05-02 15:07:00 +0530

    remaining files added

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import {client} from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+app.post('/signup',async(req,res)=>{
+    try{
+    const {username,password} = req.body;
+    const user = await client.user.create({
+        data:{
+            username,
+            password
+        }
+    })
+    res.status(201).json({message:"User created successfully",username:user.username,password:user.password});
+}catch(error){
+    res.status(500).json({message:"Internal server error"});
+}
+})
+
+app.listen(3002,()=>{
+    console.log("Server is running on port 3000");
+})
