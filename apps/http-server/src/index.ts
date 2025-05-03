02306c022f4864eec37d7b56482e316992cedf3d
akshat09022002 <akshat09022002@gmail.com>
2025-05-04 02:04:33 +0530

    testing ci-cd

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("Hello Stupid World");
 })
 
 app.post('/signup',async(req,res)=>{
